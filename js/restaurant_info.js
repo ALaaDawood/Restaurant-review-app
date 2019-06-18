bd8f4cc29f331b45cb1e83f239b61f30abfe18a7
ALaa Dawood <alaa.dawood99@yahoo.com>
2019-06-18 16:54:32 +0200

    refactor

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -35,21 +35,6 @@ initMap = () => {
   });
 }  
  
-/* window.initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-    }
-  });
-} */
 
 /**
  * Get current restaurant from page URL.
@@ -148,7 +133,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
-  const name = document.createElement('p');
+  const name = document.createElement('h4');
   name.innerHTML = review.name;
   li.appendChild(name);
 
@@ -157,7 +142,8 @@ createReviewHTML = (review) => {
   li.appendChild(date);
 
   const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'rate';
+  rating.innerHTML = `Rating: ${review.rating} Stars`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
